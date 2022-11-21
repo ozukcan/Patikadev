fb65f4efd1a6dbe81b468266f4d1ab4795e1e6d4
ozukcan <huseyinokeanusozukcan01@gmail.com>
2022-11-21 14:59:24 +0300

    Mantıksal Hatalar

---
+++ Mantıksal_Hatalar/Mantıksal_Hatalar/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Mantıksal_Hatalar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* try
+             {
+              Console.WriteLine("bir sayı giriniz:");
+             int sayi=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Girilen sayı:"+sayi);
+
+             }
+             catch(Exception ex)
+             {
+              Console.WriteLine("Hata:"+ex.Message.ToString());
+             }
+             finally
+             {
+              Console.WriteLine("işlem tamamlandı");
+             }
+             */
+
+            try
+            {
+                //int a=int.Parse(null);
+                //int a=int.Parse("test");
+                int a = int.Parse("123456789");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine("veri tipi uygun değil");
+                Console.WriteLine(ex);
+            }
+
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Girilen değer int veri aralığı arasında değil.");
+                Console.WriteLine(ex);
+            }
+
+            finally
+            {
+                Console.WriteLine("işlem tamamlandı");
+
+            }
+
+        }
+    }
+}
+
