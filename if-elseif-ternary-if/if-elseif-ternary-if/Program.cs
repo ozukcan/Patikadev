aaaffcf30548d71c190b4676965c84e60cd37c94
ozukcan <huseyinokeanusozukcan01@gmail.com>
2022-11-21 15:24:21 +0300

    if-else

---
+++ if-elseif-ternary-if/if-elseif-ternary-if/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_elseif_ternary_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("iyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("iyi geceler");
+            }
+
+            string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+            sonuc = time >= 6 && time <= 11 ? "günaydın" : time <= 18 ? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
